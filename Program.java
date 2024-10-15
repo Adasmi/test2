db983fe6b691e93dc1fcf2e882fccb16dcab065c
adasmi <asmigaj05@gmail.com>
2024-10-15 16:14:51 +0200

    blablabla

--- Program.java
+++ Program.java
@@ -2,12 +2,10 @@ import java.util.*;
 
 public class Program {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("ELo!");
-=======
+
         
-System.out.println("Witaj Joel tutaj byl Haha!");
->>>>>>> 7bf79d40b238289f6c6835857d2715443668c9d6
+        System.out.println("Witaj Joel tutaj byl Haha!");
+
         System.out.println("Dzisiaj jest: " + (new Date()));
     }
 }

29f2c1ca5d92170faba498724108cf62e66d64c6
adasmi <asmigaj05@gmail.com>
2024-10-15 16:07:49 +0200

    hfesoh

--- Program.java
+++ Program.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Witaj!");
+        System.out.println("ELo!");
         System.out.println("Dzisiaj jest: " + (new Date()));
     }
 }
